f016fe63f7424699ce2185b6448690165d8da213
Luanashinsato <lshinsato@usf.edu>
2025-01-24 12:08:30 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -3,3 +3,11 @@ let products = ["Tshirt", "Pants", "Hoodie", "Hat", "Shoes"];
 products.push("Jacket");
 products.pop();
 console.log("Updated Product List:", products);
+
+// Task 2: Accessing and Modifying Arrays
+let scores = [95, 80, 92, 77, 86];
+scores[1] = 85;
+console.log("Updated scores:", scores);
+let total = scores.reduce((sum, score)=> sum + score, 0);
+let average = total/scores.length;
+console.log ("Average score:", average);
