78f48aa4369aa6f25d93bfb575801008626d6c9e
Luanashinsato <lshinsato@usf.edu>
2025-01-24 11:57:33 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,5 @@
+// Task 1: Working with Arrays 
+let products = ["Tshirt", "Pants", "Hoodie", "Hat", "Shoes"];
+products.push("Jacket");
+products.pop();
+console.log("Updated Product List:", products);
